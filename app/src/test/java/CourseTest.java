ae219602cbed79b22ea54692fa2fb842eacaff57
David Hellwig <helldavi@isu.edu>
2021-11-12 17:37:30 -0700

    Changed how tests for Course class function

--- app/src/test/java/CourseTest.java
+++ app/src/test/java/CourseTest.java
@@ -1,74 +1,78 @@
 import edu.isu.cs.cs2263.hw02.data.Course;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author David Hellwig
  */
-
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CourseTest {
 
-
+    private Course course;
     /**
      * This is utterly stupid, BeforeAll and AfterAll will not work so I have to it the wrong way for now
      */
+
+    @BeforeAll
+    public void setup(){
+        course = new Course("test", "123", 1, 4);
+    }
+
+    @AfterEach
+    public void reset(){
+        course.setName("test");
+        course.setCode("123");
+        course.setCredits(4);
+        course.setNumber(1);
+    }
+
     @Test
     public void testGetCode(){
-        Course course = new Course("test", "123", 1, 4);
+
         assertEquals("123", course.getCode());
     }
 
     @Test
     public void testGetCredits(){
-        Course course = new Course("test", "123", 1, 4);
         assertEquals(4, course.getCredits());
     }
 
     @Test
     public void testGetNumber(){
-        Course course = new Course("test", "123", 1, 4);
         assertEquals(1, course.getNumber());
     }
 
     @Test
     public void testSetNumber(){
-        Course course = new Course("test", "123", 1, 4);
         course.setNumber(420);
         assertEquals(420, course.getNumber());
     }
 
     @Test
     public void testGetName(){
-        Course course = new Course("test", "123", 1, 4);
         assertEquals("test", course.getName());
     }
 
     @Test
     public void testSetCredits(){
-        Course course = new Course("test", "123", 1, 4);
         course.setCredits(20);
         assertEquals(20, course.getCredits());
     }
 
     @Test
     public void testSetCode(){
-        Course course = new Course("test", "123", 1, 4);
         course.setCode("321");
         assertEquals("321", course.getCode());
     }
 
     @Test
     public void testToString(){
-        Course course = new Course("test", "123", 1, 4);
         assertEquals("123 1 test (4)", course.toString());
     }
 
     @Test
     public void testSetName(){
-        Course course = new Course("test", "123", 1, 4);
         course.setName("This is the wrong way to test");
         assertEquals("This is the wrong way to test", course.getName());
     }
@@ -78,6 +82,6 @@ public class CourseTest {
         Course test = new Course();
         assertNotNull(test);
     }
-    
+
 
 }
