636c18df7d9a1ba99bcda0099fddd0df1917cc1a
David Hellwig <helldavi@isu.edu>
2021-11-15 11:12:15 -0700

    headache

--- app/src/test/java/AppTest.java
+++ app/src/test/java/AppTest.java
@@ -1,29 +1,55 @@
 import edu.isu.cs.cs2263.hw02.App;
 import edu.isu.cs.cs2263.hw02.data.Course;
+import javafx.application.Platform;
+import javafx.scene.Node;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.stage.Stage;
 import lombok.ToString;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.testfx.api.FxAssert;
+import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.framework.junit5.Start;
+import org.testfx.api.FxAssert.*;
+import java.util.concurrent.TimeoutException;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.api.FxRobot;
+import org.testfx.assertions.api.Assertions;
+import org.testfx.matcher.control.LabeledMatchers;
+import org.testfx.util.WaitForAsyncUtils;
 
 /**
  * @author David Hellwig
  */
 
+
 public class AppTest extends ApplicationTest{
 
-    App Apple;
-
-
-
-    @BeforeAll
-    public void setup(){
-        Apple = new App();
+    @BeforeEach
+    public void runAppToTests() throws Exception {
+        FxToolkit.registerPrimaryStage();
+        FxToolkit.setupApplication(App::new);
+        FxToolkit.showStage();
+        WaitForAsyncUtils.waitForFxEvents(100);
     }
+
+    @AfterEach
+    public void stopApp() throws TimeoutException {
+        FxToolkit.cleanupStages();
+    }
+
+
+    public void fuck(FxRobot r){
+        FxAssert.verifyThat(".display", LabeledMatchers.hasText("Display (dept.)"));
+    }
+
+
+
 
 
 
--- app/src/test/java/AppViewTest.java
+++ app/src/test/java/AppViewTest.java
@@ -1,10 +1,47 @@
+import edu.isu.cs.cs2263.hw02.App;
+import edu.isu.cs.cs2263.hw02.views.AppView;
+import javafx.scene.Node;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import static org.mockito.Mockito.*;
+
+import org.testfx.api.FxAssert;
+import org.testfx.assertions.api.*;
 import static org.junit.jupiter.api.Assertions.*;
+
 
 /**
  * @author David Hellwig
  */
 
-
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class AppViewTest {
+
+    private App app;
+
+    private AppView appView;
+
+
+    @BeforeAll
+    public void setUp(){
+       appView = new AppView(app) {
+           @Override
+           public void initView() {
+
+           }
+
+           @Override
+           public void updateData() {
+
+           }
+       };
+
+    }
+
+    @Test
+    public void testGetView(){
+
+    }
 }
+
